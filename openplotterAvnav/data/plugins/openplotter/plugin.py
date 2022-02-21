8d486af354d210c0bc0a070307c3bc4620a528af
e-sailing <e.minus.sailing@gmail.com>
2022-02-21 13:28:55 +0100

    use api settings

--- openplotterAvnav/data/plugins/openplotter/plugin.py
+++ openplotterAvnav/data/plugins/openplotter/plugin.py
@@ -4,15 +4,9 @@ class Plugin:
 
 	@classmethod
 	def pluginInfo(cls):
-	  return {
-	    'description': 'openplotter plugin adjust avnav for easy use and transparent thema',
-	    }
-	
-	def run(self):
-		self.api.registerLayout("op", "openplotter.json")
-		self.api.setStatus('NMEA','running')
-		while not self.api.shouldStopMainThread():
-			time.sleep(1)
+		return {
+		'description': 'openplotter plugin adjust avnav for easy use and transparent thema',
+		}
 
 	def __init__(self,api):
 		"""
@@ -24,4 +18,21 @@ class Plugin:
 		"""
 		self.api = api # type: AVNApi
 		#we register an handler for API requests
-		self.userAppId=self.api.registerUserApp(api.getBaseUrl()+"/plugin.html","openplotter-48.png")
+		#self.userAppId=self.api.registerUserApp(api.getBaseUrl()+"/plugin.html","openplotter-48.png")
+		self.api.registerRestart(self.stop) #allow the user to disable the plugin
+
+	def stop(self):
+		pass
+
+	def run(self):
+		self.api.registerLayout("op", "openplotter.json")
+		self.api.registerSettingsFile("op6","op_mobile.json")
+		self.api.registerSettingsFile("op6T","op_mobile_transp.json")
+		self.api.registerSettingsFile("op10T","op_tablet_transp.json")
+		self.api.registerSettingsFile("op20T","op_pcmonitor_transp.json")
+
+		self.api.log("started")
+		self.api.setStatus('NMEA','running')
+		while not self.api.shouldStopMainThread():
+			time.sleep(1)
+
